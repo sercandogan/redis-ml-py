ebd71df314bb0cc9e376ca2b331cd7e4173ab903
Sercan Doğan <sercandogan@yandex.com>
2018-09-25 23:05:44 +0300

    Add Matrix Operations

--- redisml/matrix.py
+++ redisml/matrix.py
@@ -8,19 +8,34 @@ class Matrix(object):
     """
 
     def __init__(self, matrix_name, conn):
+        """
+        Stores matrices and performs matrix operations
+        :param matrix_name: The name of Matrix
+        :param conn: Redis connector
+        """
         self.matrix_name = matrix_name
         self.conn = conn
-        self.shape = None
-        self.dtype = None
 
     def set(self, matrix):
+        """
+        Sets the matrix.
+        :param matrix: numpy array/matrix or list
+        :return: True or False
+        """
         arr = np.array(matrix)
         shape = self.get_shape(arr)
         nrow = shape[0]
         ncol = shape[1]
-        self.conn.execute_command(MatrixCommand.SET, self.matrix_name, nrow, ncol, *arr.flatten())
+        command = self.conn.execute_command(MatrixCommand.SET, self.matrix_name, nrow, ncol, *arr.flatten())
+        if command:
+            return True
+        return False
 
     def get(self):
+        """
+        Gets the stored matrix as Numpy Array then reshapes it.
+        :return: Numpy array
+        """
         data = self.conn.execute_command(MatrixCommand.GET, self.matrix_name)
 
         if data:
@@ -46,3 +61,59 @@ class Matrix(object):
             shape = (1, shape[0])
 
         return shape
+
+    def add(self, matrix_1, matrix_2):
+        """
+        Adds two matrices
+        :param matrix_1: first matrix
+        :param matrix_2: second matrix
+        :return: True or False
+        """
+        if not isinstance(matrix_1, Matrix) or not isinstance(matrix_2, Matrix):
+            raise Exception("Arguments' type must be Matrix")
+
+        # Matrix dimensions control by Redis
+        # if matrix_1.shape == matrix_2.shape
+
+        command = self.conn.execute_command(MatrixCommand.ADD, matrix_1.matrix_name, matrix_2.matrix_name,
+                                            self.matrix_name)
+
+        if command:
+            return True
+        return False
+
+    def multiply(self, matrix_1, matrix_2):
+        """
+        Multiplies two matrices.
+        :param matrix_1: first matrix
+        :param matrix_2: second matrix
+        :return: True or False
+        """
+        if not isinstance(matrix_1, Matrix) or not isinstance(matrix_2, Matrix):
+            raise Exception("Arguments' type must be Matrix")
+
+        # Matrix dimensions are controled by Redis.
+        # if matrix_1.shape[1] == matrix_2.shape[0]
+
+        command = self.conn.execute_command(MatrixCommand.MULTIPLY, matrix_1.matrix_name, matrix_2.matrix_name,
+                                            self.matrix_name)
+
+        if command:
+            return True
+        return False
+
+    def scale(self, scalar):
+        """
+        Scales a matrix.
+        Updates the entries of the matrix stored in key by multiplying them with scalar.
+        :param scalar: Scalar: int or float
+        :return: True or False
+        """
+        if not isinstance(scalar, int) or isinstance(scalar, float):
+            raise Exception('Scalar should be int or float')
+
+        command = self.conn.execute_command(MatrixCommand.SCALE, self.matrix_name, scalar)
+
+        if command:
+            return True
+        return False
